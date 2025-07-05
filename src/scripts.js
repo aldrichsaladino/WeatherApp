54fefc75f80014ec4014c28d470740104e346da7
Aldrich Saladino <113498599+aldrichsaladino@users.noreply.github.com>
2025-07-05 10:00:05 -0600

    started the folders and the files within

---
+++ src/scripts.js
@@ -0,0 +1,16 @@
+/* Script for the Weather App. 
+    This script will handle the fetching of weather data from an API, 
+    processing the data, and displaying it in the HTML.
+*/
+
+//Identify the elements in the HTML
+
+
+// Get Weather Data from API
+
+
+// Process Weather Data from Form
+
+
+
+// Display Weather Data in HTML
