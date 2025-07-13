2d090d567d76179f4e7a069fdd5bd545c44b1ec6
Aldrich Saladino <113498599+aldrichsaladino@users.noreply.github.com>
2025-07-13 05:24:23 -0600

    added in some layout for javascript. Need to finalize the actual scripts and then add in DOM for html interface and design later

--- src/scripts.js
+++ src/scripts.js
@@ -7,10 +7,36 @@
 
 
 // Get Weather Data from API
+function getWeatherData(location) {
+    const api_key = ''
+    const api_url = ''
 
+    const response = await fetch(api_url)
+    const data = await response.json()
+    console.log(data);
 
+// Aka check fetch the data from the api, and then log it in the console. Await is used to wait for the API before logging
+}
+
+//get api data from the html form
+var form = document.getElementById('locationForm');
+var input = document.getElementById('locationInput');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the form from submitting normally
+    var location = input.value;
+
+    getWeatherData(location); // Call the function to get weather data
+})
 // Process Weather Data from Form
+function processWeatherData {
 
+
+}
 
 
 // Display Weather Data in HTML
+function displayWeatherData {
+
+
+}
